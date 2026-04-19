60b02c3fc7ffbddd8cf26787c53f98bb85cf8305
nexohubcommunity <moonnigge@gmail.com>
2026-04-19 14:13:47 +1000

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,1 +1,13 @@
+local scripts = {
+    [136801880565837] = "https://raw.githubusercontent.com/nexohubcommunity/lua/refs/heads/main/flick.lua",
+    [987654321] = "https://your-url.com/script2.lua",
+    [555555555] = "https://your-url.com/script3.lua"
+}
 
+local url = scripts[game.GameId]
+
+if url then
+    loadstring(game:HttpGet(url))()
+else
+    warn("Unsupported game.")
+end
