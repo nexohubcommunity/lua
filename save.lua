0223e1f91282757570f5fc1c4e15b28e2ee6f944
nexohubcommunity <moonnigge@gmail.com>
2026-04-19 14:08:21 +1000

    Create save.lua

---
+++ save.lua
@@ -0,0 +1,37 @@
+local Arqel = loadstring(game:HttpGet("https://raw.githubusercontent.com/Cobruhehe/expert-octo-doodle/refs/heads/main/ArqelUi.luau"))()
+
+Arqel.Appearance.Title = "Nexo Hub"
+Arqel.Appearance.Icon = "rbxassetid://9125402735"
+Arqel.Links.Discord = "Discord.gg/nexohub"
+Arqel.Storage.FileName = "Nexo_key"
+-- Arqel.Options.KeylessUi = "true"
+
+
+Arqel.Theme.Accent = Color3.fromRGB(110, 60, 255)
+Arqel.Theme.AccentHover = Color3.fromRGB(130, 90, 255)
+Arqel.Theme.Background = Color3.fromRGB(10, 10, 20)
+Arqel.Theme.Header = Color3.fromRGB(15, 15, 30)
+Arqel.Theme.Input = Color3.fromRGB(20, 20, 40)
+Arqel.Theme.Text = Color3.fromRGB(255, 255, 255)
+Arqel.Theme.TextDim = Color3.fromRGB(160, 160, 200)
+Arqel.Theme.Success = Color3.fromRGB(0, 220, 180)
+Arqel.Theme.Error = Color3.fromRGB(255, 70, 90)
+Arqel.Theme.StatusIdle = Color3.fromRGB(120, 100, 200)
+
+
+Arqel.Shop = {
+    Enabled = true,
+    Icon = "",
+    Title = "Get Premium",
+    Subtitle = "Instant delivery • 24/7 support",
+    ButtonText = "Buy",
+    Link = "jnkie.com/overview/nexohubcommunity"
+}
+
+
+Arqel:LaunchJunkie({
+    Service = "Service 1",
+    Identifier = "1079377",
+    Provider = "Cuty"
+})
+    
