991a2e32eca022a821959dd29f623a7b93992745
nexohubcommunity <moonnigge@gmail.com>
2026-04-24 19:52:03 +1000

    Update Init.lua

--- src/Init.lua
+++ src/Init.lua
@@ -1,4 +1,4 @@
-local WindUI = {
+local NexoUI = {
 	Window = nil,
 	Theme = nil,
 	Creator = require("./modules/Creator"),
@@ -26,7 +26,7 @@ local cloneref = (cloneref or clonereference or function(instance)
 	return instance
 end)
 
-WindUI.cloneref = cloneref
+NexoUI.cloneref = cloneref
 
 local HttpService = cloneref(game:GetService("HttpService"))
 local Players = cloneref(game:GetService("Players"))
@@ -37,17 +37,14 @@ local LocalPlayer = Players.LocalPlayer or nil
 
 local Package = HttpService:JSONDecode(require("../build/package"))
 if Package then
-	WindUI.Version = Package.version
+	NexoUI.Version = Package.version
 end
 
 local KeySystem = require("./components/KeySystem")
 
-local Creator = WindUI.Creator
+local Creator = NexoUI.Creator
 
 local New = Creator.New
-
---local Tween = Creator.Tween
---local ServicesModule = WindUI.Services
 
 local Acrylic = require("./utils/Acrylic/Init")
 
@@ -56,13 +53,13 @@ local ProtectGui = protectgui or (syn and syn.protect_gui) or function() end
 local GUIParent = gethui and gethui() or (CoreGui or LocalPlayer:WaitForChild("PlayerGui"))
 
 local UIScaleObj = New("UIScale", {
-	Scale = WindUI.UIScale,
+	Scale = NexoUI.UIScale,
 })
 
-WindUI.UIScaleObj = UIScaleObj
+NexoUI.UIScaleObj = UIScaleObj
 
-WindUI.ScreenGui = New("ScreenGui", {
-	Name = "WindUI",
+NexoUI.ScreenGui = New("ScreenGui", {
+	Name = "NexoUI",
 	Parent = GUIParent,
 	IgnoreGuiInset = true,
 	ScreenInsets = "None",
@@ -89,111 +86,110 @@ WindUI.ScreenGui = New("ScreenGui", {
 	}),
 })
 
-WindUI.NotificationGui = New("ScreenGui", {
-	Name = "WindUI/Notifications",
+NexoUI.NotificationGui = New("ScreenGui", {
+	Name = "NexoUI/Notifications",
 	Parent = GUIParent,
 	IgnoreGuiInset = true,
 })
-WindUI.DropdownGui = New("ScreenGui", {
-	Name = "WindUI/Dropdowns",
+NexoUI.DropdownGui = New("ScreenGui", {
+	Name = "NexoUI/Dropdowns",
 	Parent = GUIParent,
 	IgnoreGuiInset = true,
 })
-WindUI.TooltipGui = New("ScreenGui", {
-	Name = "WindUI/Tooltips",
+NexoUI.TooltipGui = New("ScreenGui", {
+	Name = "NexoUI/Tooltips",
 	Parent = GUIParent,
 	IgnoreGuiInset = true,
 })
-ProtectGui(WindUI.ScreenGui)
-ProtectGui(WindUI.NotificationGui)
-ProtectGui(WindUI.DropdownGui)
-ProtectGui(WindUI.TooltipGui)
+ProtectGui(NexoUI.ScreenGui)
+ProtectGui(NexoUI.NotificationGui)
+ProtectGui(NexoUI.DropdownGui)
+ProtectGui(NexoUI.TooltipGui)
 
-Creator.Init(WindUI)
+Creator.Init(NexoUI)
 
-function WindUI:SetParent(parent)
-	if WindUI.ScreenGui then
-		WindUI.ScreenGui.Parent = parent
+function NexoUI:SetParent(parent)
+	if NexoUI.ScreenGui then
+		NexoUI.ScreenGui.Parent = parent
 	end
-	if WindUI.NotificationGui then
-		WindUI.NotificationGui.Parent = parent
+	if NexoUI.NotificationGui then
+		NexoUI.NotificationGui.Parent = parent
 	end
-	if WindUI.DropdownGui then
-		WindUI.DropdownGui.Parent = parent
+	if NexoUI.DropdownGui then
+		NexoUI.DropdownGui.Parent = parent
 	end
-	if WindUI.TooltipGui then
-		WindUI.TooltipGui.Parent = parent
+	if NexoUI.TooltipGui then
+		NexoUI.TooltipGui.Parent = parent
 	end
 end
-math.clamp(WindUI.TransparencyValue, 0, 1)
+math.clamp(NexoUI.TransparencyValue, 0, 1)
 
-local Holder = WindUI.NotificationModule.Init(WindUI.NotificationGui)
+local Holder = NexoUI.NotificationModule.Init(NexoUI.NotificationGui)
 
-function WindUI:Notify(Config)
+function NexoUI:Notify(Config)
 	Config.Holder = Holder.Frame
-	Config.Window = WindUI.Window
-	--Config.WindUI = WindUI
-	return WindUI.NotificationModule.New(Config)
+	Config.Nexoow = NexoUI.Nexoow
+	return NexoUI.NotificationModule.New(Config)
 end
 
-function WindUI:SetNotificationLower(Val)
+function NexoUI:SetNotificationLower(Val)
 	Holder.SetLower(Val)
 end
 
-function WindUI:SetFont(FontId)
+function NexoUI:SetFont(FontId)
 	Creator.UpdateFont(FontId)
 end
 
-function WindUI:OnThemeChange(func)
-	WindUI.OnThemeChangeFunction = func
+function NexoUI:OnThemeChange(func)
+	NexoUI.OnThemeChangeFunction = func
 end
 
-function WindUI:AddTheme(LTheme)
-	WindUI.Themes[LTheme.Name] = LTheme
+function NexoUI:AddTheme(LTheme)
+	NexoUI.Themes[LTheme.Name] = LTheme
 	return LTheme
 end
 
-function WindUI:SetTheme(Value)
-	if WindUI.Themes[Value] then
-		WindUI.Theme = WindUI.Themes[Value]
-		Creator.SetTheme(WindUI.Themes[Value])
+function NexoUI:SetTheme(Value)
+	if NexoUI.Themes[Value] then
+		NexoUI.Theme = NexoUI.Themes[Value]
+		Creator.SetTheme(NexoUI.Themes[Value])
 
-		if WindUI.OnThemeChangeFunction then
-			WindUI.OnThemeChangeFunction(Value)
+		if NexoUI.OnThemeChangeFunction then
+			NexoUI.OnThemeChangeFunction(Value)
 		end
 
-		return WindUI.Themes[Value]
+		return NexoUI.Themes[Value]
 	end
 	return nil
 end
 
-function WindUI:GetThemes()
-	return WindUI.Themes
+function NexoUI:GetThemes()
+	return NexoUI.Themes
 end
-function WindUI:GetCurrentTheme()
-	return WindUI.Theme.Name
+function NexoUI:GetCurrentTheme()
+	return NexoUI.Theme.Name
 end
-function WindUI:GetTransparency()
-	return WindUI.Transparent or false
+function NexoUI:GetTransparency()
+	return NexoUI.Transparent or false
 end
-function WindUI:GetWindowSize()
-	return WindUI.Window.UIElements.Main.Size
+function NexoUI:GetWindowSize()
+	return NexoUI.Window.UIElements.Main.Size
 end
-function WindUI:Localization(LocalizationConfig)
-	return WindUI.LocalizationModule:New(LocalizationConfig, Creator)
+function NexoUI:Localization(LocalizationConfig)
+	return NexoUI.LocalizationModule:New(LocalizationConfig, Creator)
 end
 
-function WindUI:SetLanguage(Value)
+function NexoUI:SetLanguage(Value)
 	if Creator.Localization then
 		return Creator.SetLanguage(Value)
 	end
 	return false
 end
 
-function WindUI:ToggleAcrylic(Value)
-	if WindUI.Window and WindUI.Window.AcrylicPaint and WindUI.Window.AcrylicPaint.Model then
-		WindUI.Window.Acrylic = Value
-		WindUI.Window.AcrylicPaint.Model.Transparency = Value and 0.98 or 1
+function NexoUI:ToggleAcrylic(Value)
+	if NexoUI.Window and NexoUI.Window.AcrylicPaint and NexoUI.Window.AcrylicPaint.Model then
+		NexoUI.Window.Acrylic = Value
+		NexoUI.Window.AcrylicPaint.Model.Transparency = Value and 0.98 or 1
 		if Value then
 			Acrylic.Enable()
 		else
@@ -202,7 +198,7 @@ function WindUI:ToggleAcrylic(Value)
 	end
 end
 
-function WindUI:Gradient(stops, props)
+function NexoUI:Gradient(stops, props)
 	local colorSequence = {}
 	local transparencySequence = {}
 
@@ -249,24 +245,24 @@ function WindUI:Gradient(stops, props)
 	return gradientData
 end
 
-function WindUI:Popup(PopupConfig)
-	PopupConfig.WindUI = WindUI
-	return require("./components/popup/Init").new(PopupConfig, WindUI.ScreenGui.Popups)
+function NexoUI:Popup(PopupConfig)
+	PopupConfig.NexoUI = NexoUI
+	return require("./components/popup/Init").new(PopupConfig, NexoUI.ScreenGui.Popups)
 end
 
-WindUI.Themes = require("./themes/Init")(WindUI, Creator)
+NexoUI.Themes = require("./themes/Init")(NexoUI, Creator)
 
-Creator.Themes = WindUI.Themes
+Creator.Themes = NexoUI.Themes
 
-WindUI:SetTheme("Dark")
-WindUI:SetLanguage(Creator.Language)
+NexoUI:SetTheme("Dark")
+NexoUI:SetLanguage(Creator.Language)
 
-function WindUI:CreateWindow(Config)
+function NexoUI:CreateWindow(Config)
 	local CreateWindow = require("./components/window/Init")
 
 	if not RunService:IsStudio() and writefile then
-		if not isfolder("WindUI") then
-			makefolder("WindUI")
+		if not isfolder("NexoUI") then
+			makefolder("NexoUI")
 		end
 		if Config.Folder then
 			makefolder(Config.Folder)
@@ -275,20 +271,19 @@ function WindUI:CreateWindow(Config)
 		end
 	end
 
-	Config.WindUI = WindUI
-	Config.Window = WindUI.Window
-	Config.Parent = WindUI.ScreenGui.Window
+	Config.NexoUI = NexoUI
+	Config.Window = NexoUI.Window
+	Config.Parent = NexoUI.ScreenGui.Window
 
-	if WindUI.Window then
+	if NexoUI.Window then
 		warn("You cannot create more than one window")
 		return
 	end
 
 	local CanLoadWindow = true
 
-	local Theme = WindUI.Themes[Config.Theme or "Dark"]
+	local Theme = NexoUI.Themes[Config.Theme or "Dark"]
 
-	--WindUI.Theme = Theme
 	Creator.SetTheme(Theme)
 
 	local hwid = gethwid or function()
@@ -341,7 +336,7 @@ function WindUI:CreateWindow(Config)
 				local isSuccess = false
 
 				for _, i in next, Config.KeySystem.API do
-					local serviceData = WindUI.Services[i.Type]
+					local serviceData = NexoUI.Services[i.Type]
 					if serviceData then
 						local args = {}
 						for _, argName in next, serviceData.Args do
@@ -373,26 +368,14 @@ function WindUI:CreateWindow(Config)
 
 	local Window = CreateWindow(Config)
 
-	WindUI.Transparent = Config.Transparent
-	WindUI.Window = Window
+	NexoUI.Transparent = Config.Transparent
+	NexoUI.Window = Window
 
 	if Config.Acrylic then
 		Acrylic.init()
 	end
 
-	-- function Window:ToggleTransparency(Value)
-	--     WindUI.Transparent = Value
-	--     WindUI.Window.Transparent = Value
-
-	--     Window.UIElements.Main.Background.BackgroundTransparency = Value and WindUI.TransparencyValue or 0
-	--     Window.UIElements.Main.Background.ImageLabel.ImageTransparency = Value and WindUI.TransparencyValue or 0
-	--     Window.UIElements.Main.Gradient.UIGradient.Transparency = NumberSequence.new{
-	--         NumberSequenceKeypoint.new(0, 1),
-	--         NumberSequenceKeypoint.new(1, Value and 0.85 or 0.7),
-	--     }
-	-- end
-
 	return Window
 end
 
-return WindUI
+return NexoUI
