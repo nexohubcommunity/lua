849fe621a31f87c7a80826ffcfad1c1d417328c9
nexohubcommunity <moonnigge@gmail.com>
2026-04-24 19:55:28 +1000

    Update Init.lua

--- src/utils/services/Init.lua
+++ src/utils/services/Init.lua
@@ -1,25 +1,4 @@
 return {
-    platoboost = {
-        Name = "Platoboost",
-        Icon = "rbxassetid://75920162824531",
-        Args = {"ServiceId", "Secret"},
-        
-        New = require("./Platoboost").New
-    },
-    pandadevelopment = {
-        Name = "Panda Development",
-        Icon = "panda",
-        Args = {"ServiceId"},
-        
-        New = require("./PandaDevelopment").New
-    },
-    luarmor = {
-        Name = "Luarmor",
-        Icon = "rbxassetid://130918283130165",
-        Args = {"ScriptId", "Discord"},
-        
-        New = require("./Luarmor").New
-    },
     junkiedevelopment = {
         Name = "Junkie Development",
         Icon = "rbxassetid://106310347705078",
@@ -27,6 +6,4 @@ return {
 
         New = require("./JunkieDevelopment").New
     },
-
-    -- other services soon...
 }
