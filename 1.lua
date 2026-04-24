15f4eda0e0cf316e3f5927db7c7facc593f5484b
nexohubcommunity <moonnigge@gmail.com>
2026-04-24 18:03:54 +1000

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,6 +1,6 @@
 local scripts = {
     [136801880565837] = "https://raw.githubusercontent.com/nexohubcommunity/lua/refs/heads/main/flick.lua",
-    [987654321] = "https://your-url.com/script2.lua",
+    [70845479499574] = "https://raw.githubusercontent.com/nexohubcommunity/lua/refs/heads/main/bitebynight.lua",
     [555555555] = "https://your-url.com/script3.lua"
 }
 
